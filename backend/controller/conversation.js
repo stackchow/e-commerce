b762a8437f3d5bcaccb08bc9a6930c382faa67da
stackchow <stackchow@gmail.com>
2024-01-01 23:38:58 +0100

    Seperated controller functions from routes

--- backend/controller/conversation.js
+++ backend/controller/conversation.js
@@ -6,14 +6,15 @@ const { isSeller, isAuthenticated } = require("../middleware/auth");
 const router = express.Router();
 
 // create a new conversation
-router.post(
-  "/create-new-conversation",
-  catchAsyncErrors(async (req, res, next) => {
+
+
+const createNewConvo = catchAsyncErrors(
+  async (req, res, next) => {
     try {
       const { groupTitle, userId, sellerId } = req.body;
-
+  
       const isConversationExist = await Conversation.findOne({ groupTitle });
-
+  
       if (isConversationExist) {
         const conversation = isConversationExist;
         res.status(201).json({
@@ -25,7 +26,7 @@ router.post(
           members: [userId, sellerId],
           groupTitle: groupTitle,
         });
-
+  
         res.status(201).json({
           success: true,
           conversation,
@@ -34,7 +35,11 @@ router.post(
     } catch (error) {
       return next(new ErrorHandler(error.response.message), 500);
     }
-  })
+  }
+)
+
+router.post(
+  "/create-new-conversation",
 );
 
 // get seller conversations
