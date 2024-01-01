f929d3d4701489e54c5616e8d2332055266d696d
stackchow <stackchow@gmail.com>
2024-01-01 23:36:47 +0100

    Change api version

--- backend/app.js
+++ backend/app.js
@@ -28,27 +28,27 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 }
 
 // import routes
-const user = require("./controller/user");
+const user = require("./routes/userRoutes");
 const shop = require("./controller/shop");
 const product = require("./controller/product");
 const event = require("./controller/event");
-const coupon = require("./controller/coupounCode");
+const coupon = require("./controller/couponCode");
 const payment = require("./controller/payment");
 const order = require("./controller/order");
 const conversation = require("./controller/conversation");
 const message = require("./controller/message");
 const withdraw = require("./controller/withdraw");
 
-app.use("/api/v2/user", user);
-app.use("/api/v2/conversation", conversation);
-app.use("/api/v2/message", message);
-app.use("/api/v2/order", order);
-app.use("/api/v2/shop", shop);
-app.use("/api/v2/product", product);
-app.use("/api/v2/event", event);
-app.use("/api/v2/coupon", coupon);
-app.use("/api/v2/payment", payment);
-app.use("/api/v2/withdraw", withdraw);
+app.use("/api/v1/user", user);
+app.use("/api/v1/conversation", conversation);
+app.use("/api/v1/message", message);
+app.use("/api/v1/order", order);
+app.use("/api/v1/shop", shop);
+app.use("/api/v1/product", product);
+app.use("/api/v1/event", event);
+app.use("/api/v1/coupon", coupon);
+app.use("/api/v1/payment", payment);
+app.use("/api/v1/withdraw", withdraw);
 
 // it's for ErrorHandling
 app.use(ErrorHandler);
