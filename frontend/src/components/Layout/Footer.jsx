bd557fb4503dd87bc8a78b871ac6672da1ee038e
0xaplus <codewithaplus@gmail.com>
2024-01-06 13:21:33 +0100

    Made Year dynamic

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -13,6 +13,8 @@ import {
 } from "../../static/data";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="bg-[#000] text-white">
       <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#342ac8] py-7">
@@ -110,7 +112,7 @@ const Footer = () => {
         className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10
          text-center pt-2 text-gray-400 text-sm pb-8"
       >
-        <span>© 2024 Stackchow. All rights reserved.</span>
+        <span>&copy; {currentYear} Stackchow. All rights reserved.</span>
         <span>Terms · Privacy Policy</span>
         <div className="sm:block flex items-center justify-center w-full">
           <img
