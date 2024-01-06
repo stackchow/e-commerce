6214102dcf77f8df263a4d213e37f6cb84237c27
0xaplus <codewithaplus@gmail.com>
2024-01-06 01:37:06 +0100

    Uncommented Avatar codes

--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -2,7 +2,7 @@ import { React, useState } from "react";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import styles from "../../styles/styles";
 import { Link } from "react-router-dom";
-// import { RxAvatar } from "react-icons/rx";
+import { RxAvatar } from "react-icons/rx";
 import axios from "axios";
 import { server } from "../../server";
 import { toast } from "react-toastify";
@@ -12,31 +12,31 @@ const Signup = () => {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [visible, setVisible] = useState(false);
-  // const [avatar, setAvatar] = useState(null);
+  const [avatar, setAvatar] = useState(null);
 
-  // const handleFileInputChange = (e) => {
-  //   const reader = new FileReader();
+  const handleFileInputChange = (e) => {
+    const reader = new FileReader();
 
-  //   reader.onload = () => {
-  //     if (reader.readyState === 2) {
-  //       setAvatar(reader.result);
-  //     }
-  //   };
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setAvatar(reader.result);
+      }
+    };
 
-  //   reader.readAsDataURL(e.target.files[0]);
-  // };
+    reader.readAsDataURL(e.target.files[0]);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     axios
-      .post(`${server}/user/create-user`, { name, email, password}) // Removed avatar
+      .post(`${server}/user/create-user`, { name, email, password, avatar})
       .then((res) => {
         toast.success(res.data.message);
         setName("");
         setEmail("");
         setPassword("");
-        // setAvatar();
+        setAvatar();
       })
       .catch((error) => {
         toast.error(error.response.data.message);
