3048ed0e9ae11ad808cd99f6b3dab2d551709aad
0xaplus <codewithaplus@gmail.com>
2024-02-18 23:55:56 +0100

    Refactor

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
-export const server =
-  process.env.NODE_ENV !== "PRODUCTION" //Not in production
-    ? "http://localhost:8000/api/v1"
-    : "https://api-stackmart.onrender.com/api/v1";
+// export const server = "http://localhost:8000/api/v1";
+
+// In production
+export const server = "https://api-stackmart.onrender.com/api/v1";
