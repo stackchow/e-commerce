f62f4a90bbd0975b0bd93457283014857e3afa64
0xaplus <codewithaplus@gmail.com>
2024-03-23 18:32:43 +0100

    change currency to NAIRA

--- frontend/src/components/cart/Cart.jsx
+++ frontend/src/components/cart/Cart.jsx
@@ -137,10 +137,10 @@ const CartSingle = ({ data, quantityChangeHandler, removeFromCartHandler }) => {
         <div className="pl-[5px]">
           <h1>{data.name}</h1>
           <h4 className="font-[400] text-[15px] text-[#00000082]">
-            ${data.discountPrice} * {value}
+            ₦{data.discountPrice} * {value}
           </h4>
           <h4 className="font-[600] text-[17px] pt-[3px] text-[#d02222] font-Roboto">
-            US${totalPrice}
+            ₦{totalPrice}
           </h4>
         </div>
         <RxCross1
