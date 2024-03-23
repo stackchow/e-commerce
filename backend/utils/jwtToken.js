4712a10c359cd35596d832adb3418ab6e7c61913
0xaplus <codewithaplus@gmail.com>
2024-03-23 18:32:00 +0100

    send token in res

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,18 +1,12 @@
-// create token and saving that in cookies
-const sendToken = (res, user) => {
+const sendToken = (user, statusCode, res) => {
   const token = user.getJwtToken();
 
-  // Options for cookies
-  const options = {
-    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
-    httpOnly: true,
-    // sameSite: "none",
-    sameSite: 'strict', // Prevent CSRF attacks
-    secure: true,
-    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-  };
-
-  res.cookie("token", token, options)
+  res.status(statusCode).send({
+    success: true,
+    status: statusCode,
+    user,
+    token,
+  });
 };
 
 module.exports = sendToken;
